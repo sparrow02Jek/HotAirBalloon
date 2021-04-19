57ed940d57c13555903e84d894a288d40354d3ec
iluxandro01 <68388374+iluxandro01@users.noreply.github.com>
2021-04-19 18:59:11 +0300

    test
    test

---
+++ HotAirBalloon/Assets/CubeManup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            transform.position = new Vector2(transform.position.x + 15,0);
+        }
+    }
+}
